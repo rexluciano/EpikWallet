6f623e2ae9f811ef7c6d5488c2f059047ad0949b
Rex Luciano <64059802+rexllc@users.noreply.github.com>
2022-06-02 23:20:09 +0800

    Update Server.java

--- epikwallet-core/src/main/java/com/epikwallet/core/Server.java
+++ epikwallet-core/src/main/java/com/epikwallet/core/Server.java
@@ -2,14 +2,14 @@ package com.epikwallet.core;
 
 public class Server {
     public static final String DOMAIN_NAME                  =    "https://cloudbox-app.gq";
-	public static final String END_POINT_LOGIN              =    "/api/v1/login";
-	public static final String END_POINT_SIGNUP             =    "/api/v1/signup";
-	public static final String END_POINT_USER               =    "/api/v1/user";
-	public static final String END_POINT_BALANCE            =    "/api/v1/balance";
-	public static final String END_POINT_UPDATE_BALANCE     =    "/api/v1/updateWallet";
+    public static final String END_POINT_LOGIN              =    "/api/v1/login";
+    public static final String END_POINT_SIGNUP             =    "/api/v1/signup";
+    public static final String END_POINT_USER               =    "/api/v1/user";
+    public static final String END_POINT_BALANCE            =    "/api/v1/balance";
+    public static final String END_POINT_UPDATE_BALANCE     =    "/api/v1/updateWallet";
     public static final String END_POINT_APPLICATIONS       =    "/api/v1/applications";
     public static final String END_POINT_MERCHANT           =    "/api/v1/merchant";
     public static final String END_POINT_PURCHASE           =    "/api/v1/pay";
     public static final String END_POINT_WEB_API            =    "/api/v1/connect";
-	public static final boolean isDebug                     =    false;
+    public static final boolean isDebug                     =    false;
 }

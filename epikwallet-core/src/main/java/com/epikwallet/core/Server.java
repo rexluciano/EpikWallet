0a2bcd6f75004624d7b70b3188816eb2ec075449
Rex Luciano <64059802+rexllc@users.noreply.github.com>
2022-06-03 15:44:33 +0800

    Migrate server to Europe Central.
    CloudBox Server will no longer be used instead we're using our new server in Europe Central for a better connection experience.

--- epikwallet-core/src/main/java/com/epikwallet/core/Server.java
+++ epikwallet-core/src/main/java/com/epikwallet/core/Server.java
@@ -1,15 +1,16 @@
 package com.epikwallet.core;
 
 public class Server {
-    public static final String DOMAIN_NAME                  =    "https://cloudbox-app.gq";
-    public static final String END_POINT_LOGIN              =    "/api/v1/login";
-    public static final String END_POINT_SIGNUP             =    "/api/v1/signup";
-    public static final String END_POINT_USER               =    "/api/v1/user";
-    public static final String END_POINT_BALANCE            =    "/api/v1/balance";
-    public static final String END_POINT_UPDATE_BALANCE     =    "/api/v1/updateWallet";
-    public static final String END_POINT_APPLICATIONS       =    "/api/v1/applications";
-    public static final String END_POINT_MERCHANT           =    "/api/v1/merchant";
-    public static final String END_POINT_PURCHASE           =    "/api/v1/pay";
-    public static final String END_POINT_WEB_API            =    "/api/v1/connect";
-    public static final boolean isDebug                     =    false;
+    public static final String DOMAIN_NAME                  =    "https://europe-central.epik.gq"; //Europe Data Center to support international and local server host.
+    public static final String END_POINT_LOGIN              =    "/api/v1/login"; //API end point for Login.
+    public static final String END_POINT_SIGNUP             =    "/api/v1/signup"; //API end point for registering user.
+    public static final String END_POINT_USER               =    "/api/v1/user"; //API end point to retrieve user info.
+    public static final String END_POINT_BALANCE            =    "/api/v1/balance"; //To get current user balance.
+    public static final String END_POINT_UPDATE_BALANCE     =    "/api/v1/updateWallet"; //To update user balance: DEPRECIATED
+    public static final String END_POINT_APPLICATIONS       =    "/api/v1/applications"; //To connect to user application registered in Epik Network.
+    public static final String END_POINT_MERCHANT           =    "/api/v1/merchant"; //For merchant only.
+    public static final String END_POINT_PURCHASE           =    "/api/v1/pay"; //To send money to other user.
+    public static final String END_POINT_WEB_API            =    "/api/v1/connect"; //To connect to the server.
+    public static final String CLOUD_STORAGE_URL            =    "https://s3.epik.gq"; //For storage of EpikCloud.
+    public static final boolean isDebug                     =    false; //For debugging use only.
 }
